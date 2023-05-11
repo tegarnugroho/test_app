c9b94f5cd8b67744172660ee3771546245ee8690
Tegar <tegaranugroho@gmail.com>
2023-05-11 13:14:58 +0700

    try dart ffi

---
+++ lib/counter_bindings.dart
@@ -0,0 +1,30 @@
+import 'dart:ffi';
+import 'dart:io';
+
+typedef IncrementCFunc = Uint32 Function(Uint32);
+typedef IncrementDartFunc = int Function(int);
+typedef DecrementCFunc = Uint32 Function(Uint32);
+typedef DecrementDartFunc = int Function(int);
+
+class CounterBindings {
+  late DynamicLibrary _lib;
+  late IncrementDartFunc _increment;
+  late DecrementDartFunc _decrement;
+
+  CounterBindings() {
+    _lib = Platform.isWindows
+        ? DynamicLibrary.open('counter.dll')
+        : DynamicLibrary.open('./libcounter.so');
+
+    _increment = _lib
+        .lookup<NativeFunction<IncrementCFunc>>('increment')
+        .asFunction<IncrementDartFunc>();
+
+    _decrement = _lib
+        .lookup<NativeFunction<DecrementCFunc>>('decrement')
+        .asFunction<DecrementDartFunc>();
+  }
+
+  int increment(int value) => _increment(value);
+  int decrement(int value) => _decrement(value);
+}
---
+++ src/counter.c
@@ -0,0 +1,5 @@
+#include <stdint.h>
+
+uint32_t increment(uint32_t value) { return value + 1; }
+
+uint32_t decrement(uint32_t value) { return value - 1; }
